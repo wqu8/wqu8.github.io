f5554457440d216ee512562f1abec324cdf70ce3
wqu8 <wqu8@txstate.edu>
2023-09-12 15:02:09 -0500

    Completed Task 2. Output

--- javascript/external.js
+++ javascript/external.js
@@ -1,1 +1,8 @@
 document.write("Where is my JS code?");
+//add external.js
+window.alert(5 + 6);
+//show up windows alert 11
+window.alert('5 + 6');
+//show up windows alert 5+6
+document.write('<h1>A heading</h1>');
+document.write('<p>A sentence.</p>');
