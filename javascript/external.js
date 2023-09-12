7c2ce918ff8967bf0b5fc52e1a09ad24fbbecaed
wqu8 <wqu8@txstate.edu>
2023-09-12 15:41:36 -0500

    Completed Task 5. Conditional Statement

--- javascript/external.js
+++ javascript/external.js
@@ -1,3 +1,4 @@
+/*
 document.write("Where is my JS code?");
 //add external.js
 window.alert(5 + 6);
@@ -23,3 +24,17 @@ document.writeln("<br>");
 document.writeln(a += b);
 document.writeln("<br>");
 document.writeln(a += b);
+*/
+document.writeln("<button onclick='condition()'>Conditional Test</button>")
+function condition()
+{
+  x = confirm("Are you sure you want to proceed?");
+  if(x)
+  {
+    document.writeln("You chose Okay!");
+  }
+  else
+  {
+    document.writeln("You chose Cancel!");
+  }
+}
